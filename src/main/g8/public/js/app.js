b3df260f91c18a7f8d93a22ebc6954d2bde53c6a
Luke Taylor <luke.taylor@guardian.co.uk>
2017-02-07 14:40:55 +0000

    Fix incorrect imports in js.

--- src/main/g8/public/js/app.js
+++ src/main/g8/public/js/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from './util/configureStore';
-import { setStore } from './util/storeAccessor';
+import configureStore from './stores/configureStore';
+import { setStore } from './stores/storeAccessor';
 import { routes } from './routes';
 
 const store = configureStore();
