c4b4daafbdab1293cc46cf8065645bdc173f10e5
Luke Taylor <luke.taylor@guardian.co.uk>
2017-02-07 13:51:18 +0000

    Escape ${} in setup.sh.

--- src/main/g8/scripts/setup.sh
+++ src/main/g8/scripts/setup.sh
@@ -4,7 +4,7 @@ red='\x1B[0;31m'
 
 test $(which yarn)
 if [ $? != "0" ]; then
-    echo -e "\n\r\n\r${red}yarn not found. please install yarn from https://yarnpkg.com/${plain}\n\r"
+    echo -e "\n\r\n\r${red}yarn not found. please install yarn from https://yarnpkg.com/\${plain}\n\r"
     exit 1
 fi
 
